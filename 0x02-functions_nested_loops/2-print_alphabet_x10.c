07311fb6d223a48cc7bb9c4cf4360ec941420ab6
Oluwasegun <oluwasegunjohnson0019@gmail.com>
2023-06-21 20:14:45 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,7 @@
 
 /**
  * print_alphabet_x10 - Make alphabet x10 times
+ *
  * Return: void
  */
 
@@ -9,7 +10,9 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
+
 	i = 0;
+
 	while (i < 10)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
